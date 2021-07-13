498434c8112706fa67e4aacdabad271d41b19c2e
rully <hvsciverius@gmail.com>
2021-07-13 17:41:30 +0700

    fix profile access

--- resources/views/shared/app_navbar.blade.php
+++ resources/views/shared/app_navbar.blade.php
@@ -15,10 +15,10 @@
                 <li class="nav-item">
                     <a href="{{ route('admin::dashboard') }}" class="nav-link">Admin</a>
                 </li>
+                @endif
                 <li class="nav-item">
                     <a href="{{ route('profile::edit') }}" class="nav-link">Profil</a>
                 </li>
-                @endif
                 <li class="nav-item">
                     <button type="submit" class="btn btn-link nav-link" form="logout">Keluar</button>
                 </li>
